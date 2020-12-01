010aa18f9f998e3fac8c7c6553db1945deebbc2c
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-12-01 09:54:05 +0100

    Add project files.

---
+++ CmsIocCore/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace CmsIocCore.Models
+{
+    public interface IMyLogger
+    {
+        void Log();
+    }
+
+    class Logger : IMyLogger
+    {
+        public void Log()
+        {
+            
+        }
+    }
+
+    public interface IUserRepository
+    {
+        List<string> GetAll();
+    }
+
+    class UserRepository : IUserRepository
+    {
+        private readonly IMyLogger _logger;
+
+        public UserRepository(IMyLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public List<string> GetAll()
+        {
+            return new List<string> { "Toppen", "Boppen" };
+        }
+    }
+}
